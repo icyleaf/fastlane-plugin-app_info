891073ea1d88d35d33aebefaf76dacb3853cc714
icyleaf <icyleaf.cn@gmail.com>
2021-07-08 14:49:46 +0800

    refactor: 合并并优化代码逻辑

--- lib/fastlane/plugin/app_info/helper/app_info_helper.rb
+++ lib/fastlane/plugin/app_info/helper/app_info_helper.rb
@@ -3,6 +3,54 @@ require 'json'
 module Fastlane
   module Helper
     class AppInfoHelper
+      COMMON_COLUMNS = %w[os name release_version build_version identifier size]
+
+      def self.hash_to_columns(raw)
+        raw.each_with_object({}) do |(key, value), obj|
+          name = upcase(column_name(key, value))
+          obj[name] = object_to_column(value)
+        end
+      end
+
+      def self.raw_data(app)
+        common_columns(app).merge(platform_columns(app))
+      end
+
+      def self.platform_columns(app)
+        if app.os == 'iOS'
+          return {} unless app.mobileprovision && !app.mobileprovision.empty?
+
+          app.mobileprovision.mobileprovision.each_with_object({}) do |(key, value), hash|
+            next if key == 'DeveloperCertificates' || key == 'Name' || key == 'DER-Encoded-Profile'
+
+            hash[upcase(key)] = value
+          end
+        elsif app.os == 'Android'
+          signs = app.signs.map { |f| f.path }
+          issuers = android_certificate_issuer(app)
+          permissions = app.use_permissions
+          features = app.use_features
+
+          {
+            "MinSDKVersion" => app.min_sdk_version,
+            "TargetSDKVersion" => app.target_sdk_version,
+            "Signatures" => signs,
+            "CertificateIssuers" => issuers,
+            "UsePermissions" => permissions,
+            "UseFeatures" => features,
+          }
+        else
+          {}
+        end
+      end
+
+      def self.common_columns(app)
+        COMMON_COLUMNS.each_with_object({}) do |key, hash|
+          value = key == 'size' ? app.size(true) : app.send(key.to_sym)
+          hash[key] = value
+        end
+      end
+
       def self.object_to_column(object)
         case object
         when Hash
@@ -25,32 +73,8 @@ module Fastlane
         end
       end
 
-      def self.common_columns
-        %w[name release_version build_version identifier os size]
-      end
-
-      def self.app_to_json(app)
-        data = common_columns.each_with_object({}) do |key, obj|
-          name = key.downcase == 'os' ? key.upcase : key.split('_').map(&:capitalize).join('')
-          value = key == 'size' ? app.size(true) : app.send(key.to_sym)
-          obj[name] = value
-        end
-
-        if app.os == 'iOS' && app.mobileprovision && !app.mobileprovision.empty?
-          app.mobileprovision.mobileprovision.each do |key, value|
-            next if key == 'DeveloperCertificates'
-            data[key] = value
-          end
-        elsif app.os == 'Android'
-          data["MinSDKVersion"] = app.min_sdk_version
-          data["TargetSDKVersion"] = app.target_sdk_version
-          data["CertificateIssuers"] = android_certificate_issuer(app)
-          data["Signatures"] = app.signs.map {|sign| sign.path }
-          data["UsePermissions"] = app.use_permissions
-          data["UseFeatures"] = app.use_features
-        end
-
-        JSON.dump(data)
+      def self.upcase(key)
+        key == 'os' ? key.upcase : key.split('_').map(&:capitalize).join('')
       end
 
       def self.android_certificate_issuer(app)
